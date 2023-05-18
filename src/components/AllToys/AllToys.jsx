501946d4876f4e0dbd9419e17da30d7632a924ea
Ashrafuj Jaman <tanbingithub@.com>
2023-05-18 21:47:02 +0600

    search added

--- src/components/AllToys/AllToys.jsx
+++ src/components/AllToys/AllToys.jsx
@@ -2,48 +2,69 @@ import React, { useEffect, useState } from 'react';
 
 const AllToys = () => {
     const [addedToys, setAddedToys] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
-        fetch('http://localhost:5000/addNewToys')
+        /*     fetch('http://localhost:5000/addNewToys?limit=20') */
+        fetch(`http://localhost:5000/addNewToys?limit=20&search=${searchQuery}`)
             .then(res => res.json())
             .then(data => {
                 setAddedToys(data);
             })
-    }, [])
+    }, [searchQuery]);
+    const handleSearch = (e) => {
+        setSearchQuery(e.target.value);
+    };
     return (
-        <div className="overflow-x-auto w-full">
-            <table className="table w-full">
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Seller</th>
-                        <th>Toy Name</th>
-                        <th>Sub-category</th>
-                        <th>Price</th>
-                        <th>Available Quantity</th>
+        <div className=' mt-10'>
+            <div className='flex justify-center'>
+                <div className='text-center '>
+                    <h1>All Toys Page</h1>
+                    <div className="form-control my-5">
+                        <input type="text"
 
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {addedToys.map((toy, index) => (
-                        <tr key={index}>
-                            <td>
-                                <img src={toy.image} alt={toy.name} className="h-20 w-20  " />
-                            </td>
-                            <td>{toy.seller_name}</td>
-                            <td>{toy.name}</td>
-                            <td>{toy.subCategory}</td>
-                            <td>${toy.price}</td>
-                            <td>{toy.available_quantity}</td>
+                            placeholder="Search by Toy Name"
+                            value={searchQuery}
+                            onChange={handleSearch}
+                            className="input input-bordered w-52" />
+                    </div>
+                </div>
+            </div>
+            <div className="overflow-x-auto w-full">
 
-                            <td>
-                                <button className="btn btn-ghost btn-xs">View Details</button>
-                            </td>
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Seller</th>
+                            <th>Toy Name</th>
+                            <th>Sub-category</th>
+                            <th>Price</th>
+                            <th>Available Quantity</th>
+
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {addedToys.map((toy, index) => (
+                            <tr key={index}>
+                                <td>
+                                    <img src={toy.image} alt={toy.name} className="h-20 w-20  " />
+                                </td>
+                                <td>{toy.seller_name}</td>
+                                <td>{toy.name}</td>
+                                <td>{toy.subCategory}</td>
+                                <td>${toy.price}</td>
+                                <td>{toy.available_quantity}</td>
+
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">View Details</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
