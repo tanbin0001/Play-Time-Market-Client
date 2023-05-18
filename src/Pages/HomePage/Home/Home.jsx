fe4386e39176564d1442c87c523fa6114d9c67b8
Ashrafuj Jaman <tanbingithub@.com>
2023-05-19 05:12:51 +0600

    marquee added

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import ToysGallery from '../ToysGallery/ToysGallery';
 import ShopByCategory from '../../ShopByCategory/ShopByCategory';
-
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+
             <ToysGallery></ToysGallery>
+
             <ShopByCategory></ShopByCategory>
         </div>
     );
--- src/Pages/HomePage/ToysGallery/ToysGallery.jsx
+++ src/Pages/HomePage/ToysGallery/ToysGallery.jsx
@@ -5,45 +5,46 @@ const ToysGallery = () => {
 
         <div className='my-10 '>
             <h1 className='text-center font-extrabold  mb-5 text-3xl'>Toys Gallery</h1>
-           
-            <div class="grid grid-cols-4  ">
-                {/* image 1 */}
+            <Marquee>
+                <div class=" flex">
+                    {/* image 1 */}
 
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/c3f59s.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 2 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/1huzw7.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 3 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/ligqkj.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 4 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/typ0c4.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 5 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/ejgb6r.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 6 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/joa90y.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 7 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/wl2e2d.jpg" alt="Toy 1" class="w-full" />
-                </div>
-                {/* image 8 */}
-                <div class="bg-gray-200">
-                    <img src="https://files.catbox.moe/ejv46y.jpg" alt="Toy 1" class="w-full" />
-                </div>
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/c3f59s.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 2 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/1huzw7.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 3 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/ligqkj.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 4 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/typ0c4.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 5 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/ejgb6r.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 6 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/joa90y.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 7 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/wl2e2d.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
+                    {/* image 8 */}
+                    <div class="bg-gray-200">
+                        <img src="https://files.catbox.moe/ejv46y.jpg" alt="Toy 1" class="w-full w-96" />
+                    </div>
 
-            </div>
-            
 
+
+                </div>
+            </Marquee>
         </div>
     );
 };
