69bee7bd04dc28e6c555db7f1851b20f04d59240
Ashrafuj Jaman <tanbingithub@.com>
2023-05-21 17:25:43 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBoSXORzn5kaXd0cjBJSSMRjmJwal-HsUE",
-    authDomain: "play-time-market.firebaseapp.com",
-    projectId: "play-time-market",
-    storageBucket: "play-time-market.appspot.com",
-    messagingSenderId: "965481618609",
-    appId: "1:965481618609:web:66b5858ace651c7bd09690"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
